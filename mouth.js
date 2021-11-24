e373d96f081d6065c4b0944f1ddda20e02459788
revathypr <revathypr75@gmail.com>
2021-11-24 09:42:55 +0530

    js mouth

---
+++ mouth.js
@@ -0,0 +1,43 @@
+date=new Date()
+date_no = date.getMonth()
+
+switch(date_no){
+    case 0:
+        month="january";
+        break;
+    case 1:
+        month="feb";
+        break;
+    case 2:
+        month="mar"
+        break;
+    case 3:
+        month="apr"
+        break;
+    case 4:
+        month="may"
+        break;
+    case 5:
+        month="jun"
+        break;
+    case 6:
+        month="jul"
+        break;
+    case 7:
+        month="aug"
+        break;
+    case 8:
+        month="sep"
+        break;
+    case 9:
+        month="oct"
+        break;
+    case 10:
+        month="nov"
+        break;
+    case 11:
+        month="dec"
+        break;
+
+}
+console.log(month)
